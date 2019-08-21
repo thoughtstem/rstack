6a943b7122f430bf17dc7554d8c8d38e84fe1bed
Stephen <stephen@thoughtstem.com>
2019-08-21 11:45:14 -0700

    info file

--- info.rkt
+++ info.rkt
@@ -1,8 +1,7 @@
 #lang info
-(define collection "hub")
-(define deps '("base" "racquel"))
+(define collection "rstack")
+(define deps '("base" "racquel" "db" "urlang"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/hub.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(thoughtstem))
